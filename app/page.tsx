50d5e8b241f4072e252f97f193e91f5589e61de2
CB101b <merijnsandvliet7@gmail.com>
2024-01-11 11:04:11 +0100

    Minor styling tweak

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,11 @@ export default function Home() {
         alt="cafebean logo in brounze"
         width={200}
         height={200}
+        className="m-12"
         />
       </div>
       <div className="flex flex-col items-left">
-        <a href="/about"><span className="font-black text-4xl hover:text-color-fg-hover transition duration-75">about me</span></a>
+        <a href="/about"><span className= "font-black text-4xl hover:text-color-fg-hover transition duration-75">about me</span></a>
         <a href="/resume"><span className="font-black text-4xl hover:text-color-fg-hover transition duration-75">resumé / cv</span></a>
         <a href="/music"><span className="font-black text-4xl hover:text-color-fg-hover transition duration-75">music</span></a>
       </div>
