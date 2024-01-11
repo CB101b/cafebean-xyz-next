9ea7795c82ab1378a54bfbc78ba8fabe6add9b31
cafebean <32168988+CB101b@users.noreply.github.com>
2024-01-11 20:20:55 +0100

    Add favicon, change font size

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row justify-center items-center p-24">
+    <main className="flex min-h-screen md:flex-row flex-col justify-center items-center">
       <div>
         <Image src="/cb_brounze.svg" 
         alt="cafebean logo in brounze"
@@ -11,11 +11,11 @@ export default function Home() {
         className="m-12"
         />
       </div>
-      <div className="flex flex-col items-left">
-        <a href="/about"><span className= "font-black text-4xl hover:text-color-fg-hover transition duration-75">about me</span></a>
-        <a href="/resume"><span className="font-black text-4xl hover:text-color-fg-hover transition duration-75">resumé / cv</span></a>
-        <a href="/music"><span className="font-black text-4xl hover:text-color-fg-hover transition duration-75">music</span></a>
-      </div>
+      <nav className="flex flex-col items-left">
+        <a href="/about"><span className= "font-black text-3xl hover:text-color-fg-hover active:pl-4 transition duration-75">about me</span></a>
+        <a href="/resume"><span className="font-black text-3xl hover:text-color-fg-hover transition duration-75">resumé / cv</span></a>
+        <a href="/music"><span className="font-black text-3xl hover:text-color-fg-hover transition duration-75">music</span></a>
+      </nav>
     </main>
   )
 }
