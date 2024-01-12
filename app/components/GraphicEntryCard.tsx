e8d47fb22a64ea0a860d6e547d8620e83b95be4b
CB101b <merijnsandvliet7@gmail.com>
2024-01-13 00:39:02 +0100

    Tweak graphic showcase card size

--- app/components/GraphicEntryCard.tsx
+++ app/components/GraphicEntryCard.tsx
@@ -1,6 +1,6 @@
 export default function GraphicEntryCard({img_src, title, description}:{img_src: string, title: string, description: string}) {
     return (
-        <div className="flex flex-col w-1/2 m-4 h-full bg-fgcolor rounded-lg overflow-hidden">
+        <div className="flex flex-col m-4 bg-fgcolor rounded-lg overflow-hidden">
             <img className="flex-3" src={img_src} />
             <div className="flex-1 p-4">
                 <h2 className="text-bgcolor font-extrabold">{title}</h2>
